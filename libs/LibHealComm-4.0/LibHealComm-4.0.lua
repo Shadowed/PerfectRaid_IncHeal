e0ef87b7a8e964d17e54c18f6435baa22d39cc1c
Shadowed <shadowed.wow@gmail.com>
2009-12-08 09:20:33 -0800

    Updated LibHealComm-4.0 to 1.5.5

--- libs/LibHealComm-4.0/LibHealComm-4.0.lua
+++ libs/LibHealComm-4.0/LibHealComm-4.0.lua
@@ -1,5 +1,5 @@
 local major = "LibHealComm-4.0"
-local minor = 48
+local minor = 49
 assert(LibStub, string.format("%s requires LibStub.", major))
 
 local HealComm = LibStub:NewLibrary(major, minor)
@@ -78,39 +78,14 @@ if( not HealComm.compressGUID ) then
 			return str
 	end})
 	
-	local throttle
 	HealComm.decompressGUID = setmetatable({}, {
 		__index = function(tbl, str)
 			if( not str ) then return nil end
 			local usc = unescape(str)
 			local a, b, c, d, e, f, g, h = string.byte(usc, 1, 8)
 
-			-- Failed to decompress
+			-- Failed to decompress, silently exit
 			if( not a or not b or not c or not d or not e or not f or not g or not h ) then
-				if( not throttle or throttle < GetTime() ) then
-					-- Only give this alert once every 5 minutes
-					throttle = GetTime() + 300
-
-					-- This isn't optimal, but need checking them all is the only "real" way to find out
-					-- what GUID errored in the group
-					for guid, unit in pairs(HealComm.guidToUnit) do
-						local compressed = HealComm.compressGUID[guid]
-						compressed = string.gsub(compressed, ",", "")
-						compressed = string.gsub(compressed, ":", "")
-						compressed = compressed .. ":"
-						compressed = string.split(":", compressed)
-						
-						local decompressed = HealComm.decompressGUID[compressed]
-						if( not decompressed or decompressed ~= guid ) then
-							print(string.format("%s-%s: Had GUID failure, found source %s, %s, %s (%s, %s)", major, minor, guid, unit, compressed, str, usc))
-							return ""
-						end
-					end
-					
-					print(string.format("%s-%s: Had GUID failure, but could not find source (%s, %s)", major, minor, str, usc))
-					print(string.format("a %s, b %s, d %s, c %s, e %s, f %s, g %s, h %s", tostring(a), tostring(b), tostring(c), tostring(d), tostring(e), tostring(f), tostring(g), tostring(h)))
-				end
-				
 				return ""
 			end
 			
@@ -1462,7 +1437,6 @@ HealComm.healingModifiers = HealComm.healingModifiers or {
 	[getName(59513)] = 0.00, -- Embrace of the Vampyr
 	[getName(55593)] = 0.00, -- Necrotic Aura
 	[getName(34625)] = 0.25, -- Demolish
-	[getName(34366)] = 0.25, -- Ebon Poison
 	[getName(19716)] = 0.25, -- Gehennas' Curse
 	[getName(24674)] = 0.25, -- Veil of Shadow
 	-- Wound Poison still uses a unique spellID/spellName despite the fact that it's a static 50% reduction.
@@ -1490,6 +1464,7 @@ HealComm.healingModifiers = HealComm.healingModifiers or {
 	[getName(63038)] = 0.75, -- Dark Volley
 	[getName(52771)] = 0.75, -- Wounding Strike
 	[getName(48291)] = 0.75, -- Fetid Healing
+	[getName(34366)] = 0.75, -- Ebon Poison
 	[getName(54525)] = 0.80, -- Shroud of Darkness (This might be wrong)
 	[getName(48301)] = 0.80, -- Mind Trauma (Improved Mind Blast)
 	[getName(68391)] = 0.80, -- Permafrost, the debuff is generic no way of seeing 7/13/20, go with 20
@@ -1507,7 +1482,8 @@ HealComm.healingModifiers = HealComm.healingModifiers or {
 if( IS_BUILD30300 ) then
 	HealComm.healingModifiers[getName(70588)] = 0.90 -- Suppression (Valithria Dreamwalker NPCs?)
 	HealComm.healingModifiers[getName(69674)] = 0.50 -- Mutated Infection (Rotface)
-	HealComm.healingModifiers[getName(71473)] = 2.00 -- Essence of the Vampyr Queen (Bood Queen Lana'thel)
+	--HealComm.healingModifiers[getName(71473)] = 2.00 -- Essence of the Blood Queen (Bood Queen Lana'thel)
+	HealComm.healingModifiers[getName(71473)] = false -- This no longer gives healing apparently?
 end
 
 HealComm.healingStackMods = HealComm.healingStackMods or {
@@ -1531,9 +1507,10 @@ local healingStackMods, selfModifiers = HealComm.healingStackMods, HealComm.self
 local healingModifiers, currentModifiers = HealComm.healingModifiers, HealComm.currentModifiers
 
 local distribution
+local CTL = ChatThrottleLib
 local function sendMessage(msg)
 	if( distribution ) then
-		SendAddonMessage(COMM_PREFIX, msg, distribution)
+		CTL:SendAddonMessage("BULK", COMM_PREFIX, msg, distribution)
 	end
 end
 
